3fc5386fa53e6f3bf0bff850dfb0128a9dcb66fe
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-08-29 09:40:23 +0200

    connected to Mongo Atlas

--- app/index.js
+++ app/index.js
@@ -3,10 +3,14 @@ import path from "path";
 import cors from "cors";
 import mongoose from "mongoose";
 import projectRoute from "./routes/projectRoute.js";
-const PORT = 8080;
-const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.json());
 app.use(cors());
