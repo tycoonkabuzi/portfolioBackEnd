39ef8af1196939d1b41af4dbd420bddb3f9dcd9e
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-08-29 10:08:04 +0200

    cors installed

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://kabuzintwali.com",
+  })
+);
 app.use("/projects", projectRoute);
 app.listen(PORT, () => {
   console.log("started at port " + PORT);
